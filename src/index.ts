7a6f9b91b761a2ed471ca2f1d5e4abecb2700924
Rod Bromberg <brombergrod@gmail.com>
2022-12-28 15:39:26 -0500

    init

---
+++ src/index.ts
@@ -0,0 +1,99 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import { sendSms } from './sendSms'
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+const PORT = 8080
+
+
+
+app.post('/text', async (req: Request, res: Response) => {
+    const { name, phone, message } = req.body
+    const newMessage = `${message} sent by ${name} at ${phone}`
+    sendSms(phone, newMessage)
+    res.status(201).send({
+        message: newMessage
+    })
+})
+
+// Create user endpoint
+  app.post('/users', (req, res) => {
+    const { email, password, phone } = req.body;
+    const user = {
+      email,
+      password,
+      phone
+    };
+    // userDatabase.push(user);  
+    // const welcomeMessage = 'Welcome to Chillz! Your verification code is 54875';
+    sendSms(user.phone, req.body.message)
+  
+    res.status(201).send({
+      message: req.body.message,
+      data: user
+    })
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express, { Request, Response } from 'express'
+// import { sendSms } from './sendSms'
+// const bodyParser = require('body-parser');
+// const app = express();
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use(bodyParser.json());
+
+
+// const userDatabase = [];
+// const PORT = 8080
+
+// // Create user endpoint
+// app.post('/users', (req, res) => {
+//     const { email, password, phone } = req.body;
+//     const user = {
+//       email,
+//       password,
+//       phone
+//     };
+  
+//     userDatabase.push(user);
+  
+//     const welcomeMessage = req.body.message
+  
+//     sendSms(user.phone, welcomeMessage);
+
+//     res.status(201).send({
+//         message: req.body.message,
+//     })
+  
+//     res.status(201).send({
+//       message: 'Account created successfully, kindly check your phone to activate your account!',
+//       data: user
+//     })
+//   });
+  
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
